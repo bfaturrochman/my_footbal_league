c30fa1820b596aa72e7627d5a6475a9b97fbe75a
bfaturrochman <fabagasz@gmail.com>
2025-05-07 22:28:10 +0700

    update

--- my_football_league/settings.py
+++ my_football_league/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# Inisialisasi django-environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Baca file .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2(jbl(_sp*=c&c73%5jc5jcmhi$h#zecm)akp4ifh=5-qv$^a&'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
